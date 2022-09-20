68b9e0e5970b844a4960aab8d957958b34c22bda
nebula1989 <benwalterscoding@gmail.com>
2022-09-19 19:48:17 -0700

    can view articles by source

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.news_source_index, name='news_source_index'),
+
+    path('articles/<str:source>', views.list_of_articles, name='list_of_articles'),
 ]
--- news/views.py
+++ news/views.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
 import os
+import re
 
 load_dotenv()
 
@@ -15,8 +16,8 @@ newsapi = NewsApiClient(api_key="37d8d403a48442a980e7daf82aaab00c")
 
 
 # Create your views here.
-def index(request):
-    topnews = newsapi.get_everything(q="sports")
+def list_of_articles(request, source):
+    topnews = newsapi.get_everything(sources=source)
 
     latest = topnews['articles']
     title = []
@@ -26,7 +27,7 @@ def index(request):
     date = []
     content = []
 
-    for i in range(int(len(latest) / 4)):
+    for i in range(int(len(latest))):
         news = latest[i]
 
         title.append(news['title'])
@@ -39,18 +40,34 @@ def index(request):
     all_news = zip(title, desc, content, url, author, date)
 
     context = {
-        'all_news': all_news
+        'all_news': all_news,
+        'source': source
     }
 
-    return render(request, "news/index.html", context)
+    return render(request, "news/list_of_articles.html", context)
 
 
 def scrape_all_content(url):
     page = requests.get(url)
 
     soup = BeautifulSoup(page.text, "lxml")
+
     try:
-        body_elem = soup.find("div", class_="article-text")
-        return body_elem.get_text()
+        body_text = soup.find("article")
+        return body_text.get_text()
     except AttributeError:
-        pass
+        body_text = soup.find("body")
+        return body_text.get_text()
+
+
+def news_source_index(request):
+    list_of_sources = []
+    sources = newsapi.get_sources()
+    for i in sources['sources']:
+        list_of_sources.append(i['id'])
+
+    context = {
+        'list_of_sources': list_of_sources
+    }
+
+    return render(request, 'news/news_index.html', context)
